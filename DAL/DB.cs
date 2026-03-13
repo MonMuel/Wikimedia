42ea68e9e9c8d77184ab64b75bbb2957cae2548f
MonMuel <samuelbelanger93@yahoo.com>
2026-03-13 10:08:12 -0400

    Ajoutez des fichiers projet.

---
+++ DAL/DB.cs
@@ -0,0 +1,27 @@
+﻿using EmailHandling;
+using Models;
+using PhotosManager.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web.Hosting;
+
+namespace DAL
+{
+    public sealed class DB
+    {
+        #region singleton setup
+        private static readonly DB instance = new DB();
+        public static DB Instance { get { return instance; } }
+        #endregion
+
+        public static MediasRepository Medias { get; set; } = new MediasRepository();
+        public static UsersRepository Users { get; set; } = new UsersRepository();
+        public static LoginsRepository Logins { get; set; } = new LoginsRepository();
+        public static EventsRepository Events { get; set; } = new EventsRepository();
+        public static NotificationsRepository Notifications { get; set; } = new NotificationsRepository();
+        public static Repository<UnverifiedEmail> UnverifiedEmails { get; set; } = new Repository<UnverifiedEmail>();
+        public static Repository<RenewPasswordCommand> RenewPasswordCommands { get; set; } = new Repository<RenewPasswordCommand>();
+
+    }
+}
